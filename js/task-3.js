43963bc7496965d9e7ba6812c49a840ec9a1986c
Oleksandr <gayrsuk@gmail.com>
2024-02-17 01:08:45 +0200

    get second home work for js course

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,28 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = Number.parseFloat(content);
-    const paddingWidth = Number.parseFloat(padding);
-    const borderWidth = Number.parseFloat(border);
+function checkForSpam(message) {
+  const separators = [',', '?', '!', ';', ':', '.', '[', ']'];
+  const regex = new RegExp(`[${separators.map(escapeRegExp).join('')}]`, 'g');
+  const cleanedText = message.replace(regex, '');
+  const messageArray = cleanedText.toLowerCase().split(' ');
+  let result = false;
 
-    return contentWidth + (paddingWidth * 2) + (borderWidth * 2);
+  for (let i = 0; i < messageArray.length; i++) {
+    if (messageArray[i] === 'spam' || messageArray[i] === 'sale') {
+      result = true;
+      break;
+    }
+  }
+
+  return result;
 }
 
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& означає всі знайдені співпадіння
+}
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
